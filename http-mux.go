7584339fd5f0149b2c142c8729de02b76c187035
Betim Pantina <71001098+bpan0007@users.noreply.github.com>
2024-03-05 17:57:04 +0100

    testing file server

--- http-mux.go
+++ http-mux.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	corsMux := middlewareCors(mux)
-	http.ListenAndServe(":8080", corsMux)
+	// corsMux := middlewareCors(mux)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./index.html")
+	})
+
+	err := http.ListenAndServe(":8000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
+// func middlewareCors(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		w.Header().Set("Access-Control-Allow-Origin", "*")
+// 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+// 		w.Header().Set("Access-Control-Allow-Headers", "*")
+// 		if r.Method == "OPTIONS" {
+// 			w.WriteHeader(http.StatusOK)
+// 			return
+// 		}
+// 		next.ServeHTTP(w, r)
+// 	})
+// }

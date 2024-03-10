ea5b7ede21336383fca253731c7fa98368c1ae2d
Betim Pantina <pantinabetim@gmail.com>
2024-03-10 17:07:06 +0100

    adding /healthz

--- http-mux.go
+++ http-mux.go
@@ -7,26 +7,40 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	// corsMux := middlewareCors(mux)
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./index.html")
+
+	// Serve static files from the ./assets directory
+	fileServer := http.FileServer(http.Dir("./assets"))
+	// Use http.StripPrefix to remove '/assets' prefix before passing to fileServer
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fileServer))
+	mux.Handle("/app/*", http.StripPrefix("/app/*", http.FileServer(http.Dir("/"))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
 	})
 
-	err := http.ListenAndServe(":8000", mux)
+	// func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+	// 	w.WriteHeader(http.StatusOK)
+	// 	w.Write([]byte("This is the about page."))
+	// }
+	corsMux := middlewareCors(mux)
+	err := http.ListenAndServe(":8080", corsMux)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-// func middlewareCors(next http.Handler) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Set("Access-Control-Allow-Origin", "*")
-// 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-// 		w.Header().Set("Access-Control-Allow-Headers", "*")
-// 		if r.Method == "OPTIONS" {
-// 			w.WriteHeader(http.StatusOK)
-// 			return
-// 		}
-// 		next.ServeHTTP(w, r)
-// 	})
-// }
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
